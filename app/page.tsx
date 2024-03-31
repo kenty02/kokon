0b910a6fd2802d335b326eabb39eb698556a54ee
kenty02 <42216299+kenty02@users.noreply.github.com>
2024-03-31 19:01:00 +0900

    fix: add login redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+import { createClient } from "@/utils/supabase/server";
 import { Text } from "@yamada-ui/react";
+import { redirect } from "next/navigation";
 
 export default async function Index() {
+	const supabase = createClient();
+
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+
+	if (!user) {
+		return redirect("/login");
+	}
 	return (
 		<div>
 			<Text>チームを選んでください</Text>
